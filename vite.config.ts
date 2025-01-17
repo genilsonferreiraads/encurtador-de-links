0b58471e649725950c56e61bc704239016f29328
genilson <genilsonferreiranegocios@gmail.com>
2025-01-16 21:04:45 -0300

    Adiciona configuração de variáveis de ambiente no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY)
+  }
 }) 
